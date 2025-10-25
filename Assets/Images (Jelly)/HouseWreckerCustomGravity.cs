298540e0545c07776e642cabd2d9ad08693abc60
Jacob <jrprecourt@icloud.com>
2025-10-25 18:32:04 -0400

    Fixed Merge Conflicts

--- Assets/Images (Jelly)/HouseWreckerCustomGravity.cs
+++ Assets/Images (Jelly)/HouseWreckerCustomGravity.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class CustomGravity : MonoBehaviour
+public class HouseWreckerCustomGravity : MonoBehaviour
 {
     // Gravity Scale editable on the inspector
     // providing a gravity scale per object
